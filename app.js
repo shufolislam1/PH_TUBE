f9c284126e0a6ae52bb468dbd437d6b4e945db79
shufolislam1 <shufolislam1255@gmail.com>
2023-11-29 21:37:27 +0600

    almost done

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-const loadAll = ()=>{
-    fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
-    .then(res => res.json())
-    .then(data => displayData(data.data))
-    // alert('done');
+const loadCatagory = () => {
+    fetch(`https://openapi.programming-hero.com/api/videos/categories`)
+      .then(res => res.json())
+      .then(data => {
+        const categoryIds = data.data.map(singleId => singleId.category_id);
+        attachEventListeners(categoryIds);
+      })
+      .catch(error => console.error('Error fetching categories:', error));
+  };
+
+
+const attachEventListeners = (categoryIds) => {
+    document.querySelectorAll('.category-btn').forEach((button, index) => {
+      button.addEventListener('click', function () {
+        // console.log(categoryIds);
+        const categoryId = categoryIds[index];
+        // console.log(categoryId);
+        loadAll(categoryId);
+      });
+    });
+  };
+
+const loadAll = (id)=>{
+    // console.log('fullllllll', id);
+    try {
+        fetch(`https://openapi.programming-hero.com/api/videos/category/${id?id:1000}`)
+        .then(res => res.json())
+        .then(data => displayData(data.data))
+    } catch (error) {
+        console.log("erroor", error);
+    }
+
 }
+
 
 const displayData = (data) => {
     const cardContainer = document.getElementById('card-container')
+
+    // clearing already loaded data... (ei line tar jonno ami 2 ghonta boisa aasi. mane ami kotttttto boro ekta gadha)
+    cardContainer.innerHTML = '';
+    // *********************8****8**8*****
     data.forEach(singelData => {
-        console.log(singelData);
+        // console.log(singelData.category_id);
         const card = document.createElement('div');
         card.classList.add('col-lg-3')
         card.classList.add('p-4')
@@ -39,7 +71,13 @@ background: url(<path-to-image>), lightgray 50% / cover no-repeat;" src=${singel
               font-style: normal;
               font-weight: 400;
               line-height: normal;
-              ">${singelData.authors[0].profile_name} <span>${singelData.authors[0].verified}</span>  </p>
+              ">${singelData.authors[0].profile_name} 
+              <span style="width: 20px;
+          height: 20px;
+          flex-shrink: 0;">
+          ${singelData.authors[0].verified ? '<img src="./Design in png/tik.png" style="" alt="">' : ''}
+          </span>
+              </p>
               <p style="color: rgba(23, 23, 23, 0.70);
               font-family: Inter;
               font-size: 14px;
@@ -47,13 +85,18 @@ background: url(<path-to-image>), lightgray 50% / cover no-repeat;" src=${singel
               font-weight: 400;
               line-height: normal;">${singelData.others.views} views</p>
           </div>
+          
+          
       </div>
         </div>
       </div>
         
         `
         cardContainer.appendChild(card)
+        
     });
 }
 
+
+loadCatagory()
 loadAll()
