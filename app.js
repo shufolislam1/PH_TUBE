a1d00450e1c6ffd2d2a6ac5c62b1ed20a684cca4
shufolislam1 <shufolislam1255@gmail.com>
2023-11-29 16:28:38 +0600

    img showed

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const loadAll = ()=>{
     fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayData(data.data))
+
+
     // alert('done');
 }
+
+const displayData = (data) => {
+    const cardContainer = document.getElementById('card-container')
+    data.forEach(singelData => {
+        console.log(singelData);
+        const card = document.createElement('div');
+        card.classList.add('col-lg-3')
+        card.classList.add('p-4')
+        card.classList.add('text-center')
+        card.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src=${singelData.thumbnail} class="card-img-top" alt="...">
+        <div class="card-body">
+          <div>
+          <div>
+              <img src="" alt="" srcset="">
+          </div>
+          <div>
+              <h1></h1>
+              <p></p>
+          </div>
+      </div>
+        </div>
+      </div>
+        
+        `
+        cardContainer.appendChild(card)
+    });
+}
+
