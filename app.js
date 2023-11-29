a3ef4711f4bf8f0102618a7ca5cae669bc20ab6c
shufolislam1 <shufolislam1255@gmail.com>
2023-11-29 17:20:15 +0600

    80% done

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const loadAll = ()=>{
     fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
     .then(res => res.json())
     .then(data => displayData(data.data))
-
-
     // alert('done');
 }
 
@@ -16,16 +14,38 @@ const displayData = (data) => {
         card.classList.add('p-4')
         card.classList.add('text-center')
         card.innerHTML = `
-        <div class="card" style="width: 18rem;">
-        <img src=${singelData.thumbnail} class="card-img-top" alt="...">
+        <div class="card" style="width: 18rem; border: none;">
+        <img style="border-radius: 10px" src=${singelData.thumbnail} class="card-img-top" alt="...">
         <div class="card-body">
+          <div class="d-flex mt-3">
           <div>
-          <div>
-              <img src="" alt="" srcset="">
+              <img style="width: 40px;
+              height: 40px;
+              flex-shrink: 0;
+              border-radius: 40px;
+background: url(<path-to-image>), lightgray 50% / cover no-repeat;" src=${singelData.authors[0].profile_picture} alt="" srcset="">
           </div>
-          <div>
-              <h1></h1>
-              <p></p>
+          <div class="mx-5">
+              <p>${singelData.others.posted_date}</p>
+              <p style="color: #171717;
+              font-family: Inter;
+              font-size: 16px;
+              font-style: normal;
+              font-weight: 700;
+              line-height: 26px; /* 162.5% */">${singelData.title}</p>
+              <p style="color: rgba(23, 23, 23, 0.70);
+              font-family: Inter;
+              font-size: 14px;
+              font-style: normal;
+              font-weight: 400;
+              line-height: normal;
+              ">${singelData.authors[0].profile_name} <span>${singelData.authors[0].verified}</span>  </p>
+              <p style="color: rgba(23, 23, 23, 0.70);
+              font-family: Inter;
+              font-size: 14px;
+              font-style: normal;
+              font-weight: 400;
+              line-height: normal;">${singelData.others.views} views</p>
           </div>
       </div>
         </div>
@@ -36,3 +56,4 @@ const displayData = (data) => {
     });
 }
 
+loadAll()
